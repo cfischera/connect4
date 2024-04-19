305eb594e7ca6a298e72f973cfade07af53f8c69
Colin Fischer <cfischera17@gmail.com>
2024-04-19 12:17:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ boardImage.src = "images/board.png"
 const playButton    = document.getElementById("playButton");
 const display       = document.getElementById("display");
 let columnButtons = document.getElementById("columnButtons");
+console.log(columnButtons);
 for(let i=0;i<7;i++) {
     let temp = document.createElement("button");
     temp.textContent = i.toString(10);
